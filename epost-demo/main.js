da3a6be8a3aad0b68b384835acb1b8ccca8c1b62
kuangwk <kuangweike@gmail.com>
2014-08-23 20:48:26 +0800

    add epost demo

---
+++ epost-demo/main.js
@@ -0,0 +1,36 @@
+// $('.page').on('touchstart', function (a) {
+//   console.log(a)
+//   console.log(a.touches[0].pageX);
+// })
+
+
+
+// $('.page').on('touchend', function (event) {
+//   console.log(event.pageX);
+// })
+console.log(TweenMax)
+
+pageHeight = window.innerHeight;
+var currentHeight = 0;
+
+var snap = {
+  y: function (endHeight) {
+    endHeight = Math.abs(endHeight);
+    if (Math.abs(endHeight - currentHeight) / pageHeight >= 0.5) {
+      if (endHeight - currentHeight > 0) {
+        currentHeight += pageHeight;
+        return currentHeight;
+      } else {
+        currentHeight -= pageHeight;
+        return currentHeight
+      }
+    } else {
+      return currentHeight;
+    }
+  }
+}
+
+Draggable.create('main', {type:'scrollTop',
+                          edgeResistance:0.8,
+                          throwProps:true,
+                          snap: snap});
